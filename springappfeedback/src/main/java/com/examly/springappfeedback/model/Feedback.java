8330e70608da4599a171708b964ac9343d55d60d
arjunvirtusa2023 <arjunpatidar@virtusa.com>
2025-05-27 10:39:54 +0000

    added

--- springappfeedback/src/main/java/com/examly/springappfeedback/model/Feedback.java
+++ springappfeedback/src/main/java/com/examly/springappfeedback/model/Feedback.java
@@ -1,16 +1,4 @@
 package com.examly.springappfeedback.model;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class Feedback {
 
 }
